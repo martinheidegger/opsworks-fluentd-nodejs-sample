1c3d66078f68ed87a2032dee534835b752e240c8
Martin Heidegger <mh@ikkyotech.com>
2014-06-14 12:00:53 +0900

    Added simple fluentd logger

---
+++ server.js
@@ -0,0 +1,58 @@
+"use strict";
+
+var express = require("express"),
+    app = express(),
+    fluent = require('fluent-logger'),
+    monitor,
+    access, 
+    fatal,
+    sendInterval = -1;
+
+function createLogger(name) {
+    var logger = fluent.createFluentSender(name);
+    logger.on("error", function () {
+        console.log("error while connecting to [" + name + "]");
+    });
+    return logger;
+}
+
+monitor = createLogger('monitor');
+access = createLogger('access');
+fatal = createLogger('fatal');
+
+function stopSendInterval() {
+    if (sendInterval !== -1) {
+        clearInterval(sendInterval);
+        sendInterval = -1;
+    }
+}
+
+function monitorRps(rps) {
+    monitor.emit("info", {"metric":"worker.rps", "value":rps});
+}
+
+app.get('/start', function (req, res) {
+    var rps = parseInt(req.query.rps || "20", 10),
+        rpts = rps / 10;
+    stopSendInterval();
+    sendInterval = setInterval(function () {
+        for (var i = rpts - 1; i >= 0; i--) {
+            access.emit("info", "another request happened! [" + i + "]");
+        };
+    }, 100);
+    monitorRps(rps);
+    res.send("OK");
+});
+
+app.get('/stop', function (req, res) {
+    stopSendInterval();
+    monitorRps(0);
+    res.send("OK");
+});
+
+app.get('/err', function (req, res) {
+    fatal.emit("error", {message:"This is a horrible error", stack: new Error().stack});
+    res.send("OK");
+});
+
+app.listen(80);
